5afbe3ddc9e1a2ddd098d5253d03965d202cfdf2
(no author) <(no author)@f2e92c9b-f110-0410-8fa8-b724fd9af65c>
2004-01-31 19:06:32 +0000

    This commit was manufactured by cvs2svn to create tag 'cl-irc_upstream_version_0_5_1'.
    git-svn-id: svn://common-lisp.net/project/cl-irc/svn@18 f2e92c9b-f110-0410-8fa8-b724fd9af65c

---
+++ tags/cl-irc_upstream_version_0_5_1/event.lisp
@@ -0,0 +1,175 @@
+;;;; $Id$
+;;;; $Source$
+
+;;;; See LICENSE for licensing information.
+
+(in-package :irc)
+
+(defgeneric irc-message-event (message)
+  (:documentation "Upon receipt of an IRC message from the
+connection's stream, irc-message-event will be called with the
+message."))
+
+(defmethod irc-message-event ((message irc-message))
+  (apply-to-hooks message)
+  (client-log (connection message) message "UNHANDLED-EVENT:"))
+
+(defmethod default-hook ((message irc-rpl_whoisuser-message))
+  (let ((user (find-user (connection message)
+                         (second (arguments message))))
+        (realname (trailing-argument message))
+        (username (third (arguments message)))
+        (hostname (fourth (arguments message))))
+    (setf (realname user) realname)
+    (setf (username user) username)
+    (setf (hostname user) hostname)))
+
+(defmethod default-hook ((message irc-rpl_list-message))
+  (let ((connection (connection message))
+        (channel (second (arguments message)))
+        (user-count (parse-integer (or (third (arguments message)) "0")))
+        (topic (trailing-argument message)))
+    (add-channel connection (or (find-channel connection channel)
+                                (make-channel :name channel
+                                              :topic topic
+                                              :user-count user-count)))))
+
+(defmethod default-hook ((message irc-rpl_topic-message))
+  (setf (topic (find-channel (connection message)
+                             (second (arguments message))))
+        (trailing-argument message)))
+
+(defmethod default-hook ((message irc-rpl_namreply-message))
+  (let* ((connection (connection message))
+         (channel (find-channel connection (car (last (arguments message))))))
+    (dolist (nickname (tokenize-string (trailing-argument message)))
+      (let ((user (find-or-make-user connection
+                                     (canonicalize-nickname nickname)
+                                     :username (user message)
+                                     :hostname (host message))))
+        (unless (equal user (user connection))
+          (add-user connection user)
+          (add-user channel user))))))
+
+(defmethod default-hook ((message irc-ping-message))
+  (pong (connection message) (trailing-argument message)))
+
+(defmethod default-hook ((message irc-join-message))
+  (let* ((connection (connection message))
+         (user (find-or-make-user
+                (connection message)
+                (source message)
+                :hostname (host message)
+                :username (user message)))
+         (channel (or (find-channel connection (trailing-argument message))
+                      (make-channel :name (trailing-argument message)))))
+    (if (self-message-p message)
+        (add-channel connection channel)
+        (progn
+          (add-user connection user)
+          (add-user channel user)))))
+
+(defmethod default-hook ((message irc-topic-message))
+  (setf (topic (find-channel (connection message)
+                             (first (arguments message))))
+        (trailing-argument message)))
+
+(defmethod default-hook ((message irc-part-message))
+  (let* ((connection (connection message))
+         (channel (find-channel connection (first (arguments message))))
+         (user (find-user connection (source message))))
+    (if (self-message-p message)
+        (remove-channel channel user)
+        (remove-user channel user))))
+
+(defmethod default-hook ((message irc-quit-message))
+  (let ((connection (connection message)))
+    (remove-user-everywhere connection (find-user connection (source message)))))
+
+(defmethod default-hook ((message irc-nick-message))
+  (let ((con (connection message)))
+    (change-nickname con (find-user con (source message))
+                     (trailing-argument message))))
+
+(defmethod default-hook ((message irc-kick-message))
+  (let* ((connection (connection message))
+         (channel (find-channel connection (first (arguments message))))
+         (user (find-user connection (second (arguments message)))))
+    (if (self-message-p message)
+        (remove-channel channel user)
+        (remove-user channel user))))
+
+(defmethod default-hook ((message ctcp-time-message))
+  (multiple-value-bind (second minute hour date month year day) (get-decoded-time)
+    (send-irc-message
+     (connection message)
+     :notice (make-ctcp-message
+              (format nil "TIME ~A"
+                      (make-time-message second minute hour date month year day)))
+     (source message))))
+
+(defmethod default-hook ((message ctcp-source-message))
+  (send-irc-message
+   (connection message)
+   :notice (make-ctcp-message
+            (format nil "SOURCE ~A:~A:~A"
+                    *download-host*
+                    *download-directory*
+                    *download-file*))
+   (source message)))
+
+(defmethod default-hook ((message ctcp-finger-message))
+  (let* ((user (user (connection message)))
+         (finger-info (if (not (zerop (length (realname user))))
+                          (realname user)
+                          (nickname user))))
+    (send-irc-message
+     (connection message)
+     :notice (make-ctcp-message
+              (format nil "FINGER ~A" finger-info))
+     (source message))))
+
+(defmethod default-hook ((message ctcp-version-message))
+  (send-irc-message
+   (connection message)
+   :notice (make-ctcp-message
+            (format nil "VERSION ~A" *ctcp-version*))
+   (source message)))
+
+(defmethod default-hook ((message ctcp-ping-message))
+  (send-irc-message
+   (connection message)
+   :notice (make-ctcp-message
+            (format nil "PING ~A" (trailing-argument message)))
+   (source message)))
+
+(defmethod irc-message-event ((message ctcp-dcc-chat-request-message))
+  (apply-to-hooks message)
+  (client-log (connection message) message))
+;  (when (automatically-accept-dcc-connections (configuration (connection message)))
+;    (let* ((user (find-user (connection message) (source message)))
+;           (args (tokenize-string (trailing-argument message)))
+;           (remote-address (hbo-to-vector-quad (parse-integer (fourth args))))
+;           (remote-port (parse-integer (fifth args) :junk-allowed t)))
+;      (push (make-dcc-connection :user user
+;                                 :remote-address remote-address
+;                                 :remote-port remote-port)
+;            *dcc-connections*))))
+  
+(defmethod irc-message-event ((message ctcp-dcc-send-request-message))
+  (apply-to-hooks message)
+  (client-log (connection message) message))
+;  (when (automatically-accept-dcc-downloads (configuration (connection message)))
+;    (let* ((user (find-user (connection message) (source message)))
+;           (args (tokenize-string (trailing-argument message)))
+;           (filename (third args))
+;           (remote-address (hbo-to-vector-quad (parse-integer (fourth args))))
+;           (remote-port (parse-integer (fifth args)))
+;           (filesize (parse-integer (sixth args) :junk-allowed t)))
+;      (let ((dcc-connection (make-dcc-connection :user user
+;                                                 :remote-address remote-address
+;                                                 :remote-port remote-port)))
+;      (with-open-file (stream filename :direction :output
+;                              :if-exists :supersede)
+;        (write-sequence (read-message-loop dcc-connection) stream))))))
+  
---
+++ tags/cl-irc_upstream_version_0_5_1/parse-message.lisp
@@ -0,0 +1,181 @@
+;;;; $Id$
+;;;; $Source$
+
+;;;; See the LICENSE file for licensing information.
+
+(in-package :irc)
+
+(defun find-reply-name (reply-number &key (reply-names *reply-names*))
+  "Numeric replies in the IRC RFCs have more meaningful names.  Given
+a numeric reply (`reply-number') this function will either return the
+symbol representing the reply or raise a continuable error
+(`no-such-reply') which gives you the opportunity to ignore the
+situation."
+  (let ((name (assoc reply-number reply-names)))
+    (if name
+        (cadr name)
+        (progn
+          (cerror "Ignore unknown reply."
+                  'no-such-reply :reply-number reply-number)
+          :unknown-reply))))
+
+(defun return-source (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+source part of the message.  Returns nil if the source part is not
+present."
+  (cut-between string #\: '(#\! #\Space) :start start))
+
+(defun return-user (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+user part of the message.  Returns nil if the user part is not
+present."
+  (cut-between string #\! '(#\@ #\Space) :start start))
+
+(defun return-host (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+host part of the message.  Returns nil if the host part is not
+present."
+  (cut-between string #\@ '(#\Space) :start start))
+
+(defun return-command (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+command part of the message.  Returns nil if the command part is not
+present."
+  (if (eql (char string start) #\Space)
+      (cut-between string #\Space '(#\Space) :start start)
+      (cut-between string nil '(#\Space) :start start :cut-extra nil)))
+
+(defun return-arguments (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+arguments part of the message as a list.  Returns nil if the arguments
+part is not present."
+  (multiple-value-bind (end-position return-argument)
+      (cut-between string nil '(#\: #\Return) :start start)
+    (values end-position (tokenize-string return-argument
+                                          :delimiters '(#\Space)))))
+
+(defun return-trailing-argument (string &key (start 0))
+  "Assuming `string' is a valid IRC message this function returns the
+trailing-argument part of the message.  Returns nil if the
+trailing-argument part is not present."
+  (cut-between string #\: '(#\Return) :start start))
+
+(defun parse-raw-message (string &key (start 0))
+  "Assuming `string' is a valid IRC message, parse the message and
+return the values in the following order:
+
+  - source
+  - user
+  - host
+  - command
+  - arguments
+  - trailing-argument
+
+Any values not present will be represented as nil."
+  (let ((index start)
+        (returns nil))
+    (dolist (function '(return-source
+                        return-user
+                        return-host
+                        return-command
+                        return-arguments
+                        return-trailing-argument))
+      (multiple-value-bind (return-index return-string)
+          (funcall function string :start index)
+        (setf index return-index)
+        (push return-string returns)))
+    (apply #'values (reverse returns))))
+
+(defun irc-error-reply-p (string)
+  "Returns t if `string' is a string-representation of an IRC error
+reply message, nil otherwise."
+  (unless (zerop (length string))
+    (if (and (every #'digit-char-p string)
+             (member (char string 0) '(#\4 #\5)))
+        t
+        nil)))
+
+(defun numeric-reply-p (string)
+  "Returns t if `string' is a string-representation of an IRC number
+reply, nil otherwise."
+  (every #'digit-char-p string))
+
+(defun ctcp-type-p (string type)
+  "Is the `string' actually a representation of the CTCP `type'?"
+  (if (string-equal (subseq string 1 (min (length string) 
+                                          (1+ (length (symbol-name type))))) 
+                    type)
+      type
+      nil))
+                                                       
+(defun dcc-type-p (string type)
+  "Is the `string' actually a representation of the DCC `type'?"
+  (case type
+    (:dcc-chat-request
+     (when (string-equal (char string 5) #\C)
+       :dcc-chat-request))
+    (:dcc-send-request
+     (when (string-equal (char string 5) #\S)
+       :dcc-send-request))
+    (otherwise nil)))
+
+(defun ctcp-message-type (string)
+  "If `string' is a CTCP message, return the type of the message or
+nil if this is a) not a CTCP message or b) a CTCP message we don't
+know about."
+  (if (or (not (stringp string))
+          (zerop (length string))
+          (not (eql (char string 0) +soh+)))
+      nil
+      (case (char string 1)
+        (#\A (ctcp-type-p string :action))
+        (#\C (ctcp-type-p string :clientinfo))
+        (#\D
+         (or (dcc-type-p string :dcc-chat-request)
+             (dcc-type-p string :dcc-send-request)))
+        (#\F (ctcp-type-p string :finger))
+        (#\P (ctcp-type-p string :ping))
+        (#\S (ctcp-type-p string :source))
+        (#\T (ctcp-type-p string :time))
+        (#\U (ctcp-type-p string :userinfo))
+        (#\V (ctcp-type-p string :version))
+        (otherwise nil))))
+
+(defun create-irc-message (string)
+  "If `string' is a valid IRC message parse it and return an object of
+the correct type with its slots prefilled according to the information
+in the message."
+  (multiple-value-bind (source user host command arguments trailing-argument)
+      (parse-raw-message string)
+    (let ((class 'irc-message)
+          (ctcp (ctcp-message-type trailing-argument)))
+      (when command
+        (cond
+          ((irc-error-reply-p command)
+           (progn
+             (setf command (find-reply-name (parse-integer command)))
+             (setf class 'irc-error-reply)))
+          ((numeric-reply-p command)
+           (progn
+             (setf command (find-reply-name (parse-integer command)))
+             (setf class (find-irc-message-class command))))
+          (t
+           (progn
+             (setf command (intern (string-upcase command)
+                                   (find-package :keyword)))
+             (setf class (find-irc-message-class command))))))
+      (when ctcp
+        (setf class (find-ctcp-message-class ctcp)))
+      (let ((instance (make-instance class
+                                     :source source
+                                     :user user
+                                     :host host
+                                     :command command
+                                     :arguments arguments
+                                     :connection nil
+                                     :trailing-argument trailing-argument
+                                     :received-time (get-universal-time)
+                                     :raw-message-string string)))
+        (when ctcp
+          (setf (ctcp-command instance) ctcp))
+        instance))))
