fed31d1ffba676dae78fa7ec75b36d8b4ae661d4
ehuelsmann <ehuelsmann@f2e92c9b-f110-0410-8fa8-b724fd9af65c>
2006-02-14 22:56:17 +0000

    Comment out non-applicable tests.
    git-svn-id: svn://common-lisp.net/project/cl-irc/svn@127 f2e92c9b-f110-0410-8fa8-b724fd9af65c

--- trunk/test/test-protocol.lisp
+++ trunk/test/test-protocol.lisp
@@ -10,8 +10,9 @@
 (defvar *nick3* "k^{]re")
 (defvar *chan1* "#liSP")
 
-(deftest normalize-nickname.1 (irc:normalize-nickname *nick1*) "kire")
-(deftest normalize-nickname.2 (irc:normalize-nickname *nick2*) "k\\[]re")
-(deftest normalize-nickname.3 (irc:normalize-nickname *nick3*) "k~[]re")
+;;normalize tests are broken because they need a connection these days
+;;(deftest normalize-nickname.1 (irc:normalize-nickname *nick1*) "kire")
+;;(deftest normalize-nickname.2 (irc:normalize-nickname *nick2*) "k\\[]re")
+;;(deftest normalize-nickname.3 (irc:normalize-nickname *nick3*) "k~[]re")
 
-(deftest normalize-channel-name.1 (irc:normalize-channel-name *chan1*) "#lisp")
+;;(deftest normalize-channel-name.1 (irc:normalize-channel-name *chan1*) "#lisp")
